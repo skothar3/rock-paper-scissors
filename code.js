7890addeafae503a5b4a44efdaf9b5a08aaac875
Sid Kothari <sid.kothari7@gmail.com>
2022-08-01 10:31:21 -0400

    Add style.css and update js to include ui

--- code.js
+++ code.js
@@ -3,79 +3,120 @@
 function getComputerChoice() {
     let rdmSelection = Math.floor(Math.random()*3) + 1;
     return rdmSelection < 2 ? "rock" : rdmSelection < 3 ? "paper" : "scissors";
+    
 }
 
 // Function to prompt and accept a user's selection of rock, paper or scissors and return it
-function getPlayerChoice() {
-    let playerChoice = prompt("Make a choice: rock, paper, or scissors","rock");
-    if(playerChoice.toLowerCase() != "rock" && playerChoice.toLowerCase() != "paper" && playerChoice.toLowerCase() != "scissors") {
-        alert("Invalid selection!");
-        getPlayerChoice();
+function getPlayerChoice(e) {
+    // Initialize str variable to store player choice from input
+    let playerChoice; 
+    
+    // Conditions to parse input and assign playerChoice value
+    if(e.type === 'keydown') {
+        if(e.key === "r") {
+            playerChoice = 'rock';
+        } else if(e.key === "p") {
+            playerChoice = 'paper';
+        } else if(e.key === "s") {
+            playerChoice = 'scissors';
+        } else playerChoice = 'none';
+    } else if(e.type === 'click') {
+        if(e.target.className.includes('rock')) {
+            playerChoice = 'rock';
+        } else if(e.target.className.includes('paper')) {
+            playerChoice = 'paper';
+        } else if(e.target.className.includes('scissors')) {
+            playerChoice = 'scissors';
+        } else playerChoice = 'none';
+    } else playerChoice = 'none';
+
+    // Return playerChoice to play a round of the game
+    return playerChoice;
+}
+function updateScore(result) {
+    // Get score and commentary element nodes
+    const playerID = document.querySelector("#playerScore");
+    const computerID = document.querySelector("#computerScore");
+    const commentaryID = document.querySelector("#commentary");
+
+    // Parse numerical scores from raw text
+    let playerScore = parseInt(playerID.textContent);
+    let computerScore = parseInt(computerID.textContent);
+
+    // Update score and commentary based on the round result
+    if(result ==='T') {
+        commentaryID.textContent = `Tie game, try again...`;
     }
-    return playerChoice.toLowerCase();
+    else if(result === 'W') {
+        playerID.textContent = `${++playerScore}`;
+        commentaryID.textContent = `You win this round...`;
+    } else if(result === 'L') {
+        computerID.textContent = `${++computerScore}`;
+        commentaryID.textContent = `Too bad you lost...`;}
+    // Conditions to declare winner and reset game scores once the player or computer reach 5 points
+    if(playerScore === 5) {
+        commentaryID.textContent = `I don't believe it... you've beaten the computers!! Huzzah!`;
+        playerID.textContent = `${0}`;
+        computerID.textContent = `${0}`;
+    } else if(computerScore === 5) {
+        commentaryID.textContent = `The computers have won again... humanity is in peril :(`;
+        playerID.textContent = `${0}`;
+        computerID.textContent = `${0}`;
+    } 
 }
 
 // Function to simulate a single round of a rock, paper, scissors game between the computer and a user
-// Returns a 0 or 1 depending on who won the round
-function playRound(playerSelection,computerSelection) {
+// Returns a W, L, or T str depending on the result
+function getRoundResult(playerChoice,computerChoice) {
+
+    // Initialize str variable to contain round result (win, loss, tie)
+    let result;
 
     // Condition for a tie game
-    if(playerSelection === computerSelection) {
-        alert("It's a tie!");
-        return null
+    if(playerChoice === computerChoice) {
+        result = 'T';
     // Conditions for other scenarios
     } else {
-        if(playerSelection === "rock") {
-            if(computerSelection === "paper") {
-                alert("Computer wins! Paper beats rock.");
-                return 0;
+        if(playerChoice === "rock") {
+            if(computerChoice === "paper") {
+                result = 'L';
             } else {
-                alert("You win! Rock beats scissors.");
-                return 1;
+                result = 'W';
             }
-        } else if(playerSelection === "paper") {
-            if(computerSelection === "rock") {
-                alert("You win! Paper beats rock.");
-                return 1;
+        } else if(playerChoice === "paper") {
+            if(computerChoice === "rock") {
+                result = 'W';
             } else {
-                alert("Computer wins! Scissors beat paper.");
-                return 0;
+                result = 'L';
             }
         } else {
-            if(computerSelection === "rock") {
-                alert("Computer wins! Rock beats scissors.");
-                return 0;
+            if(computerChoice === "rock") {
+                result = 'L';
             } else {
-                alert("You win! Scissors beat paper.");
-                return 1;
+                result = 'W';
             }
     }}
+    return result;
 }
-
-// Function to simulate a round of 5 games (best 3/5) between the computer and a user
-// Returns the result of each game, the final result and score
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let gameCount = 1;
+function playRound(e) {
+    let playerChoice = getPlayerChoice(e);
     
-    while(gameCount < 6) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let winOrLose = playRound(playerChoice,computerChoice);
-        if(winOrLose != null) {
-            winOrLose === 1 ? playerScore++ : computerScore++;
-            gameCount++;
-        }
-        alert("Current score: You - " + playerScore + " Computer - " + computerScore);
-    }
+    if(playerChoice === 'none') return;
+    else {
+        document.querySelector(`.section1 button.${playerChoice}`).classList.toggle('chosen');
+        let roundResult = getRoundResult(playerChoice,getComputerChoice());
 
-    if(playerScore < computerScore) {
-        alert("You lost the game! Final score: You - " + playerScore + " Computer - " + computerScore);
-    } else {
-        alert("You won the game! Final score: You - " + playerScore + " Computer - " + computerScore);
+        updateScore(roundResult);
+        
+        document.querySelector(`.section1 button.${playerChoice}`).classList.toggle('chosen');
     }
+    // Add class 'chosen' to the corresponding button for transform effect
+    
 }
+// Receive player input via keydown (r, p, or s)
+window.addEventListener('keydown',playRound);
 
-game();
+
+// Receive player input via clickable buttons (R, P, or S)
+const choices = document.querySelector('.section1 .buttonContainer');
+choices.addEventListener('click', playRound);
